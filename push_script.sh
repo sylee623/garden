84b840211124ef6cad5bda7b12a33c9efa092ef8
sylee623 <seoyeon0623@hanyang.ac.kr>
2025-08-07 15:42:28 +0900

    Upload source files for garden

---
+++ push_script.sh
@@ -0,0 +1,15 @@
+git add .
+git commit -m "Upload source files for garden"
+git push origin main
+bundle exec jekyll build
+
+cd _site
+
+git init
+git remote add origin https://github.com/sylee623/garden.git
+git checkout -b gh-pages
+git add .
+git commit -m "Deploy static site"
+git push origin gh-pages --force
+
+cd ..
